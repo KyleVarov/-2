f3b4530614f845717c7f82716b0acc016dd8aa71
kylevarov <kyrill.spiegl@gmail.com>
2020-04-07 23:10:27 +0900

    Statement + Decision Coverage 를 만족시키는 테스트 케이스를 구현했음

---
+++ MyRobot.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class MyRobot here.
+ *
+ * 2017605038 스피겔 크릴, 2018243144 노무라타카미치
+ * 2020/04/06
+ */
+public class MyRobot
+{
+    public static void main(String[] args){
+        double speed = 100;
+        //String signal = "red";
+        String[] TestSignal = {"green", "red", "yellow", "123"}; //실험용 테스트 데이터 
+        for (String temp : TestSignal){      
+            speed = RobotWalk(speed, temp);
+            System.out.println("Traffic light: "+ temp);
+            System.out.println("Robot speed = " + String.valueOf(speed)); 
+            speed = 100; //원래 속도로 초기화시킴 
+        }
+    }
+    public static double RobotWalk(double speed, String signal){
+        if(signal.equals("green")){
+        }else{
+            if(signal.equals("yellow")){
+            speed = speed * (1.2);
+            }else{
+                if(signal.equals("red")){
+                    speed = 0;
+                }else{
+                    System.out.println("Traffic light: 잘 못된 입력.");
+        }
+        }
+        }
+        return speed;
+    }
+}   
+    
+
