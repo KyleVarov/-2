993739f6c99b1c19d9c9967b9becda9fd6424520
kylevarov <kyrill.spiegl@gmail.com>
2020-04-09 22:26:19 +0900

    작성한 FlowChart 수정에 따라 소스코드를 수정했다

--- MyRobot.java
+++ MyRobot.java
@@ -1,6 +1,7 @@
 
 /**
- * Write a description of class MyRobot here.
+ * 로봇이 교통신호가 있는 횡단보도에 도착했을 때, 교통신호의 상태에 따라서, 
+ * 아래와 같이 로봇의 보행속도를 조절하는 모듈이다.
  *
  * 2017605038 스피겔 크릴, 2018243144 노무라타카미치
  * 2020/04/06
@@ -9,9 +10,8 @@ public class MyRobot
 {
     public static void main(String[] args){
         double speed = 100;
-        //String signal = "red";
-        String[] TestSignal = {"yellow","green", "red", "yellow", "123"}; //실험용 테스트 데이터 
-        for (String temp : TestSignal){      
+        String[] TestCases = {"green", "red", "yellow", "123"}; //실험용 테스트 데이터 
+        for (String temp : TestCases){      
             speed = RobotWalk(speed, temp);
             System.out.println("Robot speed = " + String.valueOf(speed));
             speed = 100;
@@ -20,15 +20,13 @@ public class MyRobot
     public static double RobotWalk(double speed, String signal){
         if(signal.equals("green")){ 
         }else{
-            if(signal.equals("yellow")){
-            speed = speed * (1.2);
+            if(signal.equals("red")){
+                speed = 0;
             }else{
-                if(signal.equals("red")){
-                    speed = 0;
-                }else{
-                    System.out.println("잘 못된 입력!");
-        }
-        }
+                if(signal.equals("yellow")){
+                    speed = speed * (1.2);
+                }
+            }
         }
         return speed;
     }
